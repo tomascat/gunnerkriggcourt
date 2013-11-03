4bd1512598b201aaee3a0723bfea69ec85e992ff
Sniper Girl <thilini@gmail.com>
2013-11-04 03:17:09 +1100

    a

--- hello.py
+++ hello.py
@@ -4,5 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return '<a href="hello">Hello World!</a>'
+
+@app.route('/hello')
 def hello():
-    return 'Hello World!'
+	return 'HELLO'
+
+if __name__ == '__main__':
+	app.run()
